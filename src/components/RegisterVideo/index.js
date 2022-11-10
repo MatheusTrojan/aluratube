997268a25dc54cf1b9e5593b2e2cf3eb2f0885cb
Matheus Trojan <matheustrojan23@gmail.com>
2022-11-10 19:31:02 -0300

    cria custom hook do formulario

--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -1,11 +1,36 @@
 import React from "react";
 import { StyledRegisterVideo } from "./styles";
 
+
+// Custom Hook
+function useForm(propsDoForm) {
+    const [values, setValues] = React.useState(propsDoForm.initialValues)
+
+    return {
+        values,
+        handleChange: (evento) => {
+            const value = evento.target.value; 
+            const name = evento.target.name
+            // console.log(evento.target)
+            // console.log(evento.target.name)
+            setValues({
+                ...values,
+                [name]: value,
+            });
+        },
+        clearForm() {
+            setValues({});
+        }
+    };
+}
+
 export default function RegisterVideo() {
+    const formCadastro = useForm({ 
+        initialValues: { titulo: "", url: "" }
+    });
 
     const [formVisivel, setFormVisivel] = React.useState(false);
-    console.log(formVisivel)
-    const [values, setValues] = React.useState({ titulo: "", url: ""})
+    // console.log(formVisivel)
 
     /* O que precisamos para o form funcionar:
         - pegar os dados, que precisam vir do state
@@ -23,13 +48,28 @@ export default function RegisterVideo() {
             {/*Operadores Ternários e de Curto-Circuito*/}
             {formVisivel
                 ? (
-                    <form>
+                    <form onSubmit={(evento) => {
+                        evento.preventDefault();
+                        console.log(formCadastro.values)
+                        setFormVisivel(false);
+                        formCadastro.clearForm()
+                    }}>
                         <div>
-                            <button className="close-modal" onClick={() => setFormVisivel(false)}>
+                            <button type="button" className="close-modal" onClick={() => setFormVisivel(false)}>
                                 X
                             </button>
-                            <input placeholder="Título do vídeo" value={values.titulo} />
-                            {/* <input placeholder="URL" value={values.url} /> */}
+                            <input 
+                                placeholder="Título do vídeo" 
+                                name="titulo"
+                                value={formCadastro.values.titulo} 
+                                onChange={formCadastro.handleChange}
+                            />
+                            <input 
+                                placeholder="URL" 
+                                name="url"
+                                value={formCadastro.values.url} 
+                                onChange={formCadastro.handleChange}
+                            />
                             <button type="submit">
                                 Cadastrar
                             </button>
